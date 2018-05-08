8eb144c74d88de1d750a84580417c98373a30b89
Victor Leonardo Cordero Espinoza <vcorderoespinoza@gmail.com>
2018-05-08 02:57:34 -0500

    seguridad ws

--- proyectos/recopelas/recopelas/Controllers/LoginController.cs
+++ proyectos/recopelas/recopelas/Controllers/LoginController.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using recopelas.Models;
+using RestSharp;
+using Newtonsoft.Json;
+using System.Net;
+using System.IO;
 
 namespace recopelas.Controllers
 {
@@ -10,7 +15,25 @@ namespace recopelas.Controllers
     {
         public ActionResult Index()
         {
+			ViewData["Data"] = "";
             return View ();
+        }
+      
+        [HttpPost]
+		public ActionResult Index(LoginModel login)
+        {
+			var client = new RestClient("http://localhost:8080/recopelas-ws/api/security/");          
+			var request = new RestRequest("login", Method.POST); 
+			string jsonToSend = JsonConvert.SerializeObject(login);          
+            
+			request.AddParameter("application/json; charset=utf-8", jsonToSend, ParameterType.RequestBody);
+            request.RequestFormat = DataFormat.Json;          
+
+			IRestResponse response = client.Execute(request);
+            var content = response.Content;
+
+			ViewData["Data"] = content;
+			return View();
         }
     }
 }
